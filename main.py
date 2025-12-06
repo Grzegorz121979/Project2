656f63933f70b96d1a8a8e720708f18b06443d06
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-12-06 19:18:51 +0100

    initialization function

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
 from PyQt5.QtCore import Qt
+import sys
 
 class Window(QWidget):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel("Hello, world!", self)
+        self.initialization()
+
+
+    def initialization(self):
+        self.setWindowTitle("Hello")
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.label)
+        self.setLayout(vbox)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.setStyleSheet("""
+            QLabel#label {
+                font-size: 40px;
+            }
+        """)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    win.resize(400, 600)
+    sys.exit(app.exec_())
