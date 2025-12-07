25c17f5c966c91bc762500ef1116c0a6a5b586b4
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-12-07 14:28:57 +0100

    QLineEdit, QPushButton

--- main.py
+++ main.py
@@ -6,18 +6,29 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.label = QLabel("Hello, world!", self)
+        self.line = QLineEdit(self)
+        self.button = QPushButton("Click Me", self)
         self.initialization()
 
 
     def initialization(self):
         self.setWindowTitle("Hello")
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.label)
+        vbox.addWidget(self.line)
+        vbox.addWidget(self.button)
+
         self.setLayout(vbox)
         self.label.setAlignment(Qt.AlignCenter)
+        self.line.setAlignment(Qt.AlignCenter)
+
         self.label.setObjectName("label")
+        self.line.setObjectName("line")
+        self.button.setObjectName("button")
+
         self.setStyleSheet("""
-            QLabel#label {
+            QLabel, QLineEdit, QPushButton {
                 font-size: 40px;
             }
         """)
@@ -27,5 +38,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = Window()
     win.show()
-    win.resize(400, 600)
+    # win.resize(400, 600)
     sys.exit(app.exec_())
