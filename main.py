10658cd5a57ca6f8672f34770bf8ae0248d400e3
Grzegorz121979 <pociechagrzegorz@outlook.com>
2025-12-07 14:39:58 +0100

    get_name function

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
 from PyQt5.QtCore import Qt
 import sys
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.label = QLabel("Hello, world!", self)
+        self.label = QLabel("What's your name?", self)
         self.line = QLineEdit(self)
         self.button = QPushButton("Click Me", self)
+        self.label2 = QLabel(self)
         self.initialization()
 
 
@@ -18,20 +19,32 @@ class Window(QWidget):
         vbox.addWidget(self.label)
         vbox.addWidget(self.line)
         vbox.addWidget(self.button)
+        vbox.addWidget(self.label2)
 
         self.setLayout(vbox)
         self.label.setAlignment(Qt.AlignCenter)
         self.line.setAlignment(Qt.AlignCenter)
+        self.label2.setAlignment(Qt.AlignCenter)
 
         self.label.setObjectName("label")
         self.line.setObjectName("line")
         self.button.setObjectName("button")
+        self.label2.setObjectName("label2")
 
         self.setStyleSheet("""
             QLabel, QLineEdit, QPushButton {
                 font-size: 40px;
             }
         """)
+
+        self.button.clicked.connect(self.get_name)
+
+    def get_name(self):
+        name = self.line.text().strip()
+        if name:
+            self.label2.setText(f"Hello, {name}")
+        else:
+            self.label2.setText("Enter your name")
 
 
 if __name__ == "__main__":
